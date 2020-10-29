ca9ffa318fc8332c8cc9be0e8a72a7f29f40918f
Vishal3ppp <55372287+Vishal3ppp@users.noreply.github.com>
2020-10-29 11:16:18 +0800

    Initial commit

---
+++ FileValidation/FileHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N3PS.File.Validatation.FileValidation
+{
+    class FileHelper
+    {
+        public void ValidateFile(string flatFilePath)
+        {
+            string[] lines = System.IO.File.ReadAllLines(flatFilePath);
+
+            foreach (string line in lines)
+            {
+
+
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using N3PS.File.Validatation.FileValidation;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N3PS.File.Validatation
+{
+    class Program
+    {
+
+
+      
+        static void Main(string[] args)
+        {
+
+            Logger logger = LogManager.GetCurrentClassLogger();
+            logger.Error("This is an error message");
+            logger.Info("This is an info message");
+            logger.Fatal("This is an Fatal message");
+            logger.Trace("This is an Trace message");
+            logger.Warn("This is an Warn message");
+            
+
+            //FileHelper f = new FileHelper();
+            //f.ValidateFile(@"C:\Users\vishal.chilka\Desktop\ZSB120OM.OUT");
+        }
+    }
+}
